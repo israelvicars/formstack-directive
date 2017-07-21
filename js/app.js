9b529a74b4a930998689c293b56aeda6272bd6c0
Israel Vicars <israelvicars@gmail.com>
2017-07-21 01:35:48 -0400

    Code beautification

--- js/app.js
+++ js/app.js
@@ -1,27 +1,31 @@
 const fsApp = angular.module('fsApp',[]);
 
-fsApp.directive('fsPercentage', function() {
+fsApp.directive('fsPercentage', fsPercentage);
+
+function fsPercentage() {
   return {
   	restrict: 'A',
     require: 'ngModel',
-    link: function(scope, element, attrs, modelCtrl) {
-      function percentageValidation(value) {
-        const valid = (value >= 0 && value <= 1);
-
-        modelCtrl.$setValidity('percentage', valid);
-
-        return value;
-      }
-
-      modelCtrl.$parsers.push(percentageValidation);
-
-      const input = attrs.ngModel;
-
-      scope.$watch(attrs.ngModel, (value) => {
-      	if (value >= 0 && value <= 1) {
-      		element.on("blur", () => element.val(`${Math.round(100 * value)}%`));
-      	}
-      });
-    },
+    link,
   };
-});
+
+  function link (scope, element, attrs, modelCtrl) {
+    modelCtrl.$parsers.push(percentageValidation);
+
+    scope.$watch(attrs.ngModel, (value) => {
+      if (percentValid(value)) {
+        element.on('blur', () => element.val(`${Math.round(100 * value)}%`));
+      }
+    });
+
+    function percentageValidation(value) {
+      const valid = percentValid(value);
+      modelCtrl.$setValidity('percentage', valid);
+      return value;
+    }
+
+    function percentValid(value) {
+      return (value >= 0 && value <= 1);
+    }
+  }
+};
