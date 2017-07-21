9b719d4961b4b3224dc6a8433ad90710e13f65bd
Israel Vicars <israelvicars@gmail.com>
2017-07-20 23:31:13 -0400

    Add remaining tests for separate controller implementation

--- spec/app-spec.js
+++ spec/app-spec.js
@@ -1,17 +1,36 @@
-describe('Controller: FsCtrl', () => {
+describe('Controller: FsPercentageCtrl', () => {
   let ngController;
 
   beforeEach(module('fsApp'));
 
   beforeEach(inject(function ($controller) {
-      ngController = $controller;
+    ngController = $controller;
   }));
 
-  it('displays a percentage from a float', () => {
+  it('returns valid and returns a percentage from a valid float input', () => {
     const percentageInput = 0.5;
     const $scope = { percentageInput };
     const controller = ngController('FsPercentageCtrl', { $scope });
 
+    expect($scope.percentageValid).toBe(true);
     expect($scope.percentageOutput).toEqual('50%');
+  });
+
+  it('returns invalid and returns no percentage if input is below 0', () => {
+    const percentageInput = -1.0;
+    const $scope = { percentageInput };
+    const controller = ngController('FsPercentageCtrl', { $scope });
+
+    expect($scope.percentageValid).toBe(false);
+    expect($scope.percentageOutput).toEqual('');
+  });
+
+  it('returns invalid and returns no percentage if input is above 1', () => {
+    const percentageInput = 2.0;
+    const $scope = { percentageInput };
+    const controller = ngController('FsPercentageCtrl', { $scope });
+
+    expect($scope.percentageValid).toBe(false);
+    expect($scope.percentageOutput).toEqual('');
   });
 });
